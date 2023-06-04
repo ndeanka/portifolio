7fc8afa289caf675f6c1e6aaa600611f90946d5d
ndeanka <williammlula4@gmail.com>
2023-06-04 11:03:33 +0300

    add fragement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'portifolio';
+export class AppComponent implements OnInit{
+  title = 'portfolio';
+
+  constructor(private router:Router, private activatedRoute: ActivatedRoute){}
+  ngOnInit(): void {
+    this.activatedRoute.fragment.subscribe((val ) => {
+      this.jumpTo(val);
+    })
+      
+  }
+
+  jumpTo(Selection: string | null){
+    document.getElementById(Selection as string)?.scrollIntoView({behavior: 'smooth'})
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {RouterModule, Routes} from "@angular/router";
+
+
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { WorkComponent } from './work/work.component';
 import { HeaderComponent } from './header/header.component';
 import { ServicesComponent } from './services/services.component';
+
+
+const appRouter: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: AppComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'work', component: WorkComponent},
+  {path: 'service', component: ServicesComponent},
+]
 
 @NgModule({
   declarations: [
@@ -18,7 +30,8 @@ import { ServicesComponent } from './services/services.component';
     ServicesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRouter),
   ],
   providers: [],
   bootstrap: [AppComponent]
